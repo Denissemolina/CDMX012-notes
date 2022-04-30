091de70b96df22a47bcba7309d38e74c7b175d18
DenisseMolina <dmolinaruelas@gmail.com>
2022-04-30 12:57:45 -0500

    Edit and delete notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,12 +1,11 @@
 import "./notes.css";
 import {
-  auth,
   collection,
   getDocs,
   addDoc,
   doc,
-  setDoc,
   updateDoc,
+  deleteDoc,
 } from "firebase/firestore";
 import { db } from "../lib/FirebaseConfig";
 import { useEffect, useState } from "react";
@@ -14,21 +13,32 @@ import { useEffect, useState } from "react";
 export default function Notes() {
   const [newTittle, setNewTittle] = useState("");
   const [newNote, setNewNote] = useState("");
+  const [tittleValor, updateTittle] = useState("");
+
 
   const [note, setNote] = useState([]);
   const docRef = collection(db, "Notes");
 
+  //CREAR NOTAS SI SIRVE
   const createNote = async () => {
-    await addDoc;
+    await addDoc(docRef, { Tittle: newTittle, note: newNote });
   };
 
-  const updateNote = async (id, Tittle, note) => {
-    await updateDoc(docRef, {
-      Tittle: newTittle,
-      note: newNote,
+  //BORRAR NOTA
+  const deleteNote = async (id) => {
+    const userDoc = doc(db, "Notes", id);
+    await deleteDoc(userDoc);
+  };
+
+  //ACTUALIZAR!!!!!
+  const updateNote = async (id, Tittle) => {
+    const userDoc = doc(db, "Notes", id);
+    await updateDoc(userDoc, {
+      Tittle: tittleValor,
     });
   };
 
+  []; //IMPRIMIR LAS NOTAS EN PANTALLA
   useEffect(() => {
     const impressNotes = async () => {
       const data = await getDocs(docRef);
@@ -40,43 +50,54 @@ export default function Notes() {
 
   return (
     <div className="div_notes">
-      <div>
-        <form>
-          <input
-            onChange={(event) => {
-              setNewTittle(event.target.value);
-            }}
-            placeholder="Titulo"
-          />
-          <input
-            onChange={(event) => {
-              setNewNote(event.target.value);
-            }}
-            placeholder="Escribe tu nota"
-          />
-        </form>
-        <button onClick={createNote}> Enviar </button>
-        {note.map((note) => {
-          return (
-            <div>
+      {/* SAQUE LE FORM Y SI FUNCIONA*/}
+      <input
+        placeholder="Titulo"
+        onChange={(event) => {
+          setNewTittle(event.target.value);
+        }}
+      />
+      <input
+        placeholder="Escribe tu nota"
+        onChange={(event) => {
+          setNewNote(event.target.value);
+        }}
+      />
+
+      <button onClick={createNote}> Enviar </button>
+      {note.map((note, pos) => {
+        //esto ESTABA OK Y SERA MODIFICADO PAL EDIT
+        return (
+          <div key={pos}>
+            {" "}
+            <h1>T: {note.Tittle}</h1>
+            <h2>n: {note.note}</h2>
+            <input
+              placeholder="Actualizar"
+              onChange={(event) => {
+                updateTittle(event.target.value);
+                console.log(updateTittle);
+              }}
+            />
+            <button
+              onClick={() => {
+                updateNote(note.id, note.Tittle);
+              }}
+            >
               {" "}
-              <h1>Tittle: {note.Tittle}</h1>
-              <p>note: {note.note}</p>
-            </div>
-          );
-        })}
-        <input
-          onChange={(event) => {
-            setNewNote(event.target.value);
-          }}
-          placeholder="Actualiza"
-        />
-        <button
-          onClick={ updateNote(note.id, note.Tittle)}
-        >
-          Edit
-        </button>
-      </div>
+              ZEND{" "}
+            </button>
+            <button
+              onClick={() => {
+                deleteNote(note.id);
+              }}
+            >
+              {" "}
+              BORRRA
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
